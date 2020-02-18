dd7d26420b6945541b98af054d7f77e9b89afe15
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-02-18 10:22:37 +0600

    Update output.py

--- scripts/output.py
+++ scripts/output.py
@@ -109,7 +109,8 @@ translations = [
     sdg.translations.TranslationInputSdgTranslations(source='https://github.com/open-sdg/translations-un-sdg.git', tag='1.0.0-rc1'),
     # Also pull in translations from the 'translations' folder in this repo.
     sdg.translations.TranslationInputYaml(source='translations'),
-    sdg.translations.TranslationInputCsv(source='translations')
+    sdg.translations.TranslationInputCsv(source='translations'),
+    sdg.translations.TranslationInputSdmx(source=dsd)
 ]
 
 # Create an "output" from these inputs and schema, for JSON for Open SDG.
