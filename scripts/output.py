7b403db39bdc4b5dacec50ad6a84ef9f97d1ef2b
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-02-19 05:00:23 +0000

    Update output.py

--- scripts/output.py
+++ scripts/output.py
@@ -112,8 +112,11 @@ translations = [
     sdg.translations.TranslationInputCsv(source='translations'),
     sdg.translations.TranslationInputSdmx(source=dsd)
 ]
+# Indicate any extra fields for the reporting stats, if needed.
+    reporting_status_extra_fields: ['organization_name']
+    if 'reporting_status_extra_fields' in options:
+        reporting_status_extra_fields = options['reporting_status_extra_fields']
 
-reporting_status_extra_fields: ['organization_name']
 
 # Create an "output" from these inputs and schema, for JSON for Open SDG.
 opensdg_output = sdg.outputs.OutputOpenSdg(inputs, schema, output_folder='_site', translations=translations)
