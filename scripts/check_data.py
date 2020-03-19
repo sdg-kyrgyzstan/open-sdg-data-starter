376abb437d270dcab59a8872b591c83b191a3d47
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-03-19 12:18:01 +0000

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,8 +1,17 @@
 from sdg.open_sdg import open_sdg_check
 
+# Perhaps we need to alter the data in some way.
+def alter_data(df):
+    if 'Source details' in df:
+      del df['Source details']
+    return df
+
 # Validate the indicators.
-validation_successful = open_sdg_check(config='open_sdg_config_sdmx.yml')
+validation_successful = open_sdg_check(config='open_sdg_config_sdmx.yml', alter_data=alter_data,
+    alter_meta=alter_meta)
 
 # If everything was valid, perform the build.
-if not validation_successful:
+if validation_successful:
+    open_sdg_build(config=config, alter_data=alter_data, alter_meta=alter_meta)
+else:
     raise Exception('There were validation errors. See output above.')
