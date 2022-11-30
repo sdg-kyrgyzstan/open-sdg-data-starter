9ecfc5af768b4b29a03544c537017e437f82c0e3
Nazira20 <51254145+Nazira20@users.noreply.github.com>
2022-11-30 15:22:13 +0600

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -4,5 +4,4 @@ from sdg.open_sdg import open_sdg_check
 validation_successful = open_sdg_check(config='open_sdg_config_sdmx.yml')
 
 # If everything was valid, perform the build.
-if not validation_successful:
-    raise Exception('There were validation errors.See output above.')
+if not validation_successful: raise Exception('There were validation errors.See output above.')
